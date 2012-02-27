288c533c28bdf0ba626b48f915a2e4863c132422
Erik Rumppe <erumppe@gmail.com>
2012-02-27 13:43:39 -0800

    Inital project setup

---
+++ src/java/erumppe/dbt/page/BorderPage.java
@@ -0,0 +1,11 @@
+package erumppe.dbt.page;
+
+import org.apache.click.Page;
+        
+public class BorderPage extends Page
+{
+   public String getTemplate() 
+   {
+      return "/border-template.htm";
+   }
+}
---
+++ src/java/erumppe/dbt/page/HomePage.java
@@ -0,0 +1,5 @@
+package erumppe.dbt.page;
+
+public class HomePage extends BorderPage{
+    public String title = "Home";
+}
